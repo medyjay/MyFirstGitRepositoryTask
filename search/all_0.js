c30861c48a3654d97986a8325b5bd804d45b16c2
Medy <59209459+medyjay@users.noreply.github.com>
2020-01-04 19:18:03 +0100

    Delete all_0.js

--- search/all_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['account_0',['Account',['../class_banking_app_1_1_account.html',1,'BankingApp']]],
-  ['accounttest_1',['AccountTest',['../class_banking_app_1_1_account_test.html',1,'BankingApp']]]
-];
