a6413dba8d845ace216c3abb613d72ed00d317b4
Medy <59209459+medyjay@users.noreply.github.com>
2020-01-04 19:22:16 +0100

    Delete functions_0.js

--- search/functions_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['account_30',['Account',['../class_banking_app_1_1_account.html#abcfb49619ee7498e919fff875be69514',1,'BankingApp.Account.Account(decimal Balance)'],['../class_banking_app_1_1_account.html#a3d239cecf473107e4452663144ab78bf',1,'BankingApp.Account.Account(decimal dAmount, decimal wAmount)']]]
-];
