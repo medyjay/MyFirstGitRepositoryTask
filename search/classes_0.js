6fc823063d5545d0e4d7ad4c54843848afbf1317
Medy <59209459+medyjay@users.noreply.github.com>
2020-01-04 19:21:22 +0100

    Delete classes_0.js

--- search/classes_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['account_5',['Account',['../class_banking_app_1_1_account.html',1,'BankingApp']]],
-  ['accounttest_6',['AccountTest',['../class_banking_app_1_1_account_test.html',1,'BankingApp']]]
-];
