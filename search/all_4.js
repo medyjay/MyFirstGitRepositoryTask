3c74bd7e70031478ec87d8a08dbc9c49ab44f3f2
Medy <59209459+medyjay@users.noreply.github.com>
2020-01-04 19:19:50 +0100

    Delete all_4.js

--- search/all_4.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['getbalance_12',['getBalance',['../class_banking_app_1_1_account.html#a50d79195548560aeb3d55297bb49440b',1,'BankingApp::Account']]],
-  ['getcalculateinterest_13',['getCalculateInterest',['../class_banking_app_1_1_savings_account.html#a47fd2e73b9ba2ea8ac0dbc08540851eb',1,'BankingApp::SavingsAccount']]],
-  ['getfee_14',['getFee',['../class_banking_app_1_1_checking_account.html#a3aaf49455a2c27a3c7cf2b6b14ce6a06',1,'BankingApp::CheckingAccount']]]
-];
